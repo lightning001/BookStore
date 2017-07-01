aa0d71497ee005240932889c33e43e6092688f8f
congphuong <congphuong32@gmail.com>
2017-07-01 16:04:35 +0700

    add email support

--- src/test/Test.java
+++ src/test/Test.java
@@ -14,10 +14,6 @@ public class Test {
 		// System.out.println(StatisticDAO.statisticPeriod("3/2017", "8/2017"));
 		// System.out.println(SearchDAO.searchBook("la"));
 		Accounts a = AccountDAO.getAccount(4);
-		a.setLevels(3);
-		Manager m = new Manager(a);
-		System.out.println(m);
-		System.out.println(ManagerDAO.update(m));
-		HibernateUtils.shutdown();
+		System.out.println(a);
 	}
 }
